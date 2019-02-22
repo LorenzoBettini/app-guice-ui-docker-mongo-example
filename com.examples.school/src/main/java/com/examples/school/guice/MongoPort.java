8890d4724e749653d8d69e36a869a0c9d045d9e1
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-22 11:27:46 +0100

    created binding annotations

---
+++ com.examples.school/src/main/java/com/examples/school/guice/MongoPort.java
@@ -0,0 +1,15 @@
+package com.examples.school.guice;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import com.google.inject.BindingAnnotation;
+
+@BindingAnnotation
+@Target({ FIELD, PARAMETER, METHOD })
+@Retention(RUNTIME)
+public @interface MongoPort {
+
+}
